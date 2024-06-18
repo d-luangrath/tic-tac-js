b60aa7223c74f0ab9c7609dde6ee13630be57f0f
Darla Luangrath <darla.luangrath@gmail.com>
2024-06-18 14:51:11 -0700

    Create event handlers for the sqaure

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hello index.js");
+
+function play() {
+    console.log("Square was clicked");
+  }
+  
+  const squares = document.querySelectorAll('.square');
+  
+  for (const square of squares) {
+    square.addEventListener('click', play)
+  }
